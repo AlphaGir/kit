a1b0ccfb2ccc2cd3e291d78c821091ac8325b87f
Lordmiao <1534594096@qq.com>
2019-09-09 21:18:12 +0800

    配置文件完成

--- struct.c
+++ struct.c
@@ -21,27 +21,36 @@ char *default_type;
 ser *s;
 int count;//server数量
 loc *lo;
-
+void getparamterinit(int length)
+{
+     s=(ser*)malloc(sizeof(ser)*length);
+     ser s1=(ser)malloc(sizeof(ser));
+     
+}
 int main()
 {
-    s=(ser*)malloc(sizeof(ser*));
+    /*s=(ser*)malloc(sizeof(ser)*10);
     ser s1=(ser)malloc(sizeof(ser));
     int j=10;
     int i;
-    for(i=0;i<j;i++)
-    {
-    scanf("%d",&(s1->listen));
-    s[i]=s1;
-   // s1=(ser)malloc(sizeof(ser));
- //   s1->server_name=(char*)malloc(20);
-    }
-   /* loc l1=(loc)malloc(sizeof(loc));
-    s1->l=(loc*)malloc(sizeof(loc*));
+
+    s1->listen=80;
+    s[0]=s1;
+    
+    s1->l=(loc*)malloc(sizeof(loc)*10);
+    loc l1=(loc)malloc(sizeof(loc));
+    l1->pre=(char*)malloc(sizeof(char)*25);
+    l1->root=(char*)malloc(sizeof(char)*25);
+    l1->http_proxy=(char*)malloc(sizeof(char)*25);
+    l1->index=(char*)malloc(sizeof(char)*25);
     s1->l[0]=l1;
-    strcpy("/sss/",l1->pre);
-    strcpy("ddddd",l1->http_proxy);
-    strcpy("/ss/d/",l1->index);
+    strcpy(l1->pre,"ssss");
+    strcpy(l1->http_proxy,"dddd");
+    strcpy(l1->index,"ddeee");
     */
-    for(i=0;i<j;i++)
-    printf("%d ",s[i]->listen);
+    char *ss=(char*)malloc(sizeof(char)*10);
+    if(ss==NULL)
+    printf("%s",ss);
+    else
+	    printf("not null\n");
 }
